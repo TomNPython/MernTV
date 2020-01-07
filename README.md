448be40b29ece745364ed01e5e6f0691f8499c0a
TomNPython <tom.nicholson1000@gmail.com>
2020-01-07 11:38:00 +0700

    edit component added

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Mern Full Stack Project
 
 Using the MERN stack to create an app tracking a user's watched TV series and associated ratings 
+
+ToDo:
+
+- Update create user to create series (with safeguard for pre-existing series)
+- Update create series entry to reviews
+- max out stars at 5
+- include table sort
+- adapt table to show intro but not whole, create separate pages for whole review (inc images?)
--- src/components/edit-series.component.js
+++ src/components/edit-series.component.js
@@ -24,10 +24,139 @@ export default class EditSeries extends Component {
     }
   }
 
+  componentDidMount() {
+    axios.get('http://localhost:3000/series/'+this.props.match.params.id)
+        .then(res => {
+          this.setState({
+            username: res.data.username,
+            title: res.data.title,
+            description: res.data.description,
+            stars: res.data.stars,
+            date: new Date(res.data.date),
+          }
+          )
+        })
+        .catch(err => console.log(err))
+
+    
+    axios.get('http://localhost:3000/users')
+          .then(res => {
+            this.setState({
+              users: res.data.map(user => user.username)
+            })
+          })
+          .catch(err => console.log(err))
+  };
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeStars(e) {
+    this.setState({
+      stars: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+onSubmit(e) {
+  e.preventDefault() 
+
+  const series = {
+    username: this.state.username,
+    title: this.state.title, 
+    description: this.state.description,
+    stars: this.state.stars,
+    date: this.state.date
+  }
+
+  console.log(series)
+
+  axios.post('http://localhost:3000/series/update/'+this.props.match.params.id, series)
+        .then(res => console.log(res.data))
+
+  window.location = '/'
+}
+
   render() {
     return (
       <div>
-        <p>You are on the Edit Series component!</p>
+         <h3>Edit Series Entry</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <select ref="userInput"
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}>
+                {
+                  this.state.users.map(function(user) {
+                    return <option 
+                      key={user}
+                      value={user}>{user}
+                      </option>;
+                  })
+                }
+            </select>
+          </div>
+          <div className="form-group"> 
+            <label>Title: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.title}
+                onChange={this.onChangeTitle}
+                />
+          </div>
+          <div className="form-group"> 
+            <label>Description: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                />
+          </div>
+          <div className="form-group">
+            <label>Stars: </label>
+            <input 
+                type="text" 
+                className="form-control"
+                value={this.state.stars}
+                onChange={this.onChangeStars}
+                />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <DatePicker
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+
+          <div className="form-group">
+            <input type="submit" value="Edit Series Entry" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     )
   }
