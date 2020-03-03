0e46c1a378b59b6ff213523b3c7bd530ba468932
TomNPython <tom.nicholson1000@gmail.com>
2020-03-03 09:32:23 +0700

    Updates

---
+++ precache-manifest.a72c543ab6cefd458b650246289485b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32b65b482bdb4932c41954ef82b0b88",
+    "url": "/MernTV/index.html"
+  },
+  {
+    "revision": "d069dec1314d3e707b6d",
+    "url": "/MernTV/static/css/2.23be29d4.chunk.css"
+  },
+  {
+    "revision": "6dcc5fb83a32a75c7710",
+    "url": "/MernTV/static/css/main.bb6138c2.chunk.css"
+  },
+  {
+    "revision": "d069dec1314d3e707b6d",
+    "url": "/MernTV/static/js/2.3312c176.chunk.js"
+  },
+  {
+    "revision": "d57e69aeadc1d65039037330bbaabf32",
+    "url": "/MernTV/static/js/2.3312c176.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dcc5fb83a32a75c7710",
+    "url": "/MernTV/static/js/main.f0bf9265.chunk.js"
+  },
+  {
+    "revision": "0e92eed607878f7eaa98",
+    "url": "/MernTV/static/js/runtime-main.b4cf1296.js"
+  }
+]);
